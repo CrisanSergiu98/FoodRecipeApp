b1b810326446fa53d0199da789f24a98a8e4d905
Sergiu Crisan <CDSergiu@outlook.com>
2022-10-04 22:46:05 +0300

    Initial Commit
    Adding the blank projects to the repository

---
+++ FoodRecipeUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FoodRecipeUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
