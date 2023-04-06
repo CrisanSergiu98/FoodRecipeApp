8126b9bc5ef4d16edb5da46f3579d544e1b91249
Sergiu Crisan <CDSergiu@outlook.com>
2023-04-07 00:48:28 +0300

    Added Transactions

--- FoodRecipeDataManagerLibrary/DataAccess/SqlDataAccess.cs
+++ FoodRecipeDataManagerLibrary/DataAccess/SqlDataAccess.cs
@@ -4,9 +4,12 @@ using Microsoft.Extensions.Configuration;
 using System.Data.SqlClient;
 
 namespace MinimalAPIDataAccessLibrary.DBAccess;
-public class SqlDataAccess : ISqlDataAccess 
+public class SqlDataAccess : ISqlDataAccess
 {
     private readonly IConfiguration _config;
+    private IDbConnection _connection;
+    private IDbTransaction _transaction;
+    private bool isClosed = false;
 
     public SqlDataAccess(IConfiguration config)
     {
@@ -27,5 +30,63 @@ public class SqlDataAccess : ISqlDataAccess
 
         await connection.ExecuteAsync(storedProcedure, parameters,
             commandType: CommandType.StoredProcedure);
+    }
+
+    public void StartTransaction()
+    {
+        string connectionString = _config.GetConnectionString("DefaultDevelopment");
+        _connection = new SqlConnection(connectionString);
+        _connection.Open();
+
+        _transaction = _connection.BeginTransaction();
+
+        isClosed = false;
+    }
+
+    public List<T> LoadDataInTransaction<T, U>(string storedProcedure, U parameters)
+    {
+        List<T> rows = _connection.Query<T>(storedProcedure, parameters,
+            commandType: CommandType.StoredProcedure, transaction: _transaction).ToList();
+
+        return rows;
+    }
+
+    public void SaveDataInTransaction<T>(string storedProcedure, T parameters)
+    {
+        _connection.Execute(storedProcedure, parameters,
+            commandType: CommandType.StoredProcedure, transaction: _transaction);
+    }    
+
+    public void CommitTransaction()
+    {
+        _transaction?.Commit();
+        _connection?.Close();
+        isClosed = true;
+    }
+
+    public void RollbackTransaction()
+    {
+        _transaction?.Rollback();
+        _connection?.Close();
+        isClosed = true;
+    }
+
+    public void Dispose()
+    {
+        if (isClosed == false)
+        {
+            try
+            {
+                CommitTransaction();
+            }
+            catch (Exception ex)
+            {
+                //_logger.LogError(ex, "Commit transaction failed in the dispose method.");
+                throw;
+            }
+        }
+
+        _transaction = null;
+        _connection = null;
     }
 }
